4f0eb141567d050dd22b2685230450245e704a6f
joe senenfelder <est2020.joe@gmail.com>
2023-06-25 17:49:18 -0500

    landing page 80%

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
+import Layout from '@/components/Layout'
 import Head from 'next/head'
-
+import Image from 'next/image'
+import profilePic from '../../public/images/profile/chick-ink-pic.png'
+import AnimatedText from '@/components/AnimatedText'
 
 
 export default function Home() {
@@ -12,7 +15,27 @@ export default function Home() {
         
       </Head>
      <main className='flex items-center text-dark w-full min-h-screen'>
-        HOME
+        <Layout className='pt-0'>
+
+        <div className='flex items-center justify-between w-full'>
+          <div className='w-1/2'>
+            <Image src={profilePic} alt='joe_senny' className='w-full h-auto'/>
+          </div>
+          <div className='w-1/2'>
+
+              
+                <AnimatedText text= 'Engineering Dreams into Reality Through Code and Design.' className='!text-6xl'/>
+
+                <p>
+                  As a skilled full-stack developer, I am passionate about turning creative ideas into impactful web applications. Delve into my latest projects and articles, highlighting my mastery in React.js and web development.
+                </p>
+
+          </div>
+
+        </div>
+
+        </Layout>
+
       </main>
     </>
   )
